a8aab7f7673e2b7a4bc5e445e1b25a8d565c5b97
Mahmoud Alaskndrany <ma7m0d_alx@aucegypt.edu>
2026-07-06 16:07:31 +0300

    Add models and file IO

--- Program.cs
+++ Program.cs
@@ -1,6 +1,26 @@
+using System.Text.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
 
 app.Run();
+
+async Task<List<Feed>> ReadFeedsAsync()
+{
+    if (!File.Exists("feeds.json"))
+        return [];
+
+    await using var stream = File.OpenRead("feeds.json");
+    return await JsonSerializer.DeserializeAsync<List<Feed>>(stream) ?? [];
+}
+
+async Task WriteFeedsAsync(List<Feed> feeds)
+{
+    await using var stream = File.Create("feeds.json");
+    await JsonSerializer.SerializeAsync(stream, feeds, new JsonSerializerOptions { WriteIndented = true });
+}
+
+record Feed(string Id, string Url, string Title);
+record FeedDto(string Url);
+record Article(string FeedTitle, string Title, string Link, DateTime PublishDate, string Summary);
